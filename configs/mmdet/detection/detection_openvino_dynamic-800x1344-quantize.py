893bad8cfabf7c5736b46da4f2f0075311ad8d59
humu789 <humu@pjlab.org.cn>
2023-03-07 17:50:12 +0800

    update quant config

--- configs/mmdet/detection/detection_openvino_dynamic-800x1344-quantize.py
+++ configs/mmdet/detection/detection_openvino_dynamic-800x1344-quantize.py
@@ -1,26 +1,24 @@
 _base_ = ['../_base_/base_openvino_dynamic-800x1344.py']
 
+global_qconfig = dict(
+    w_observer=dict(type='mmrazor.PerChannelMinMaxObserver'),
+    a_observer=dict(type='mmrazor.MovingAverageMinMaxObserver'),
+    w_fake_quant=dict(type='mmrazor.FakeQuantize'),
+    a_fake_quant=dict(type='mmrazor.FakeQuantize'),
+    w_qscheme=dict(
+        qdtype='qint8', bit=8, is_symmetry=True, is_symmetric_range=True),
+    a_qscheme=dict(qdtype='quint8', bit=8, is_symmetry=True),
+)
+
 # TODO save in mmrazor's checkpoint
-quantizer = dict(
-    type='mmrazor.OpenvinoQuantizer',
-    skipped_methods=[
-        'mmdet.models.dense_heads.base_dense_head'
-        '.BaseDenseHead.predict_by_feat',
-    ],
-    prepare_custom_config_dict=None,
-    convert_custom_config_dict=None,
-    qconfig=dict(
-        qtype='affine',
-        w_observer=dict(type='mmrazor.MSEObserver'),
-        a_observer=dict(type='mmrazor.EMAMSEObserver'),
-        w_fake_quant=dict(type='mmrazor.FakeQuantize'),
-        a_fake_quant=dict(type='mmrazor.FakeQuantize'),
-        w_qscheme=dict(
-            bit=8,
-            is_symmetry=True,
-            is_per_channel=True,
-            is_pot_scale=False,
-        ),
-        a_qscheme=dict(
-            bit=8, is_symmetry=False, is_per_channel=False,
-            is_pot_scale=False)))
+quantizer=dict(
+    type='mmrazor.OpenVINOQuantizer',
+    global_qconfig=global_qconfig,
+    tracer=dict(
+        type='mmrazor.CustomTracer',
+        skipped_methods=[
+            'mmdet.models.dense_heads.yolox_head.YOLOXHead.predict_by_feat',  # noqa: E501
+            'mmdet.models.dense_heads.yolox_head.YOLOXHead.loss_by_feat',
+        ]))
+
+checkpoint='/mnt/petrelfs/humu/experiments/yolox-s_openvino/model_ptq_deploy.pth'
--- configs/mmdet/detection/detection_tensorrt-int8_dynamic-320x320-1344x1344_explicit.py
+++ configs/mmdet/detection/detection_tensorrt-int8_dynamic-320x320-1344x1344_explicit.py
@@ -11,9 +11,9 @@ backend_config = dict(
         dict(
             input_shapes=dict(
                 input=dict(
-                    min_shape=[1, 3, 64, 64],
-                    opt_shape=[1, 3, 640, 640],
-                    max_shape=[1, 3, 800, 800])))
+                    min_shape=[1, 3, 320, 320],
+                    opt_shape=[1, 3, 800, 1344],
+                    max_shape=[1, 3, 1344, 1344])))
     ])
 
 global_qconfig = dict(
@@ -27,14 +27,27 @@ global_qconfig = dict(
         qdtype='qint8', bit=8, is_symmetry=True, averaging_constant=0.1),
 )
 
+# retinanet
+# quantizer=dict(
+#     type='mmrazor.TensorRTQuantizer',
+#     global_qconfig=global_qconfig,
+#     tracer=dict(
+#         type='mmrazor.CustomTracer',
+#         skipped_methods=[
+#             'mmdet.models.dense_heads.base_dense_head.BaseDenseHead.predict_by_feat',  # noqa: E501
+#             'mmdet.models.dense_heads.anchor_head.AnchorHead.loss_by_feat',
+#         ]))
+
+# yolox
 quantizer=dict(
     type='mmrazor.TensorRTQuantizer',
     global_qconfig=global_qconfig,
     tracer=dict(
         type='mmrazor.CustomTracer',
         skipped_methods=[
-            'mmdet.models.dense_heads.base_dense_head.BaseDenseHead.predict_by_feat',  # noqa: E501
-            'mmdet.models.dense_heads.anchor_head.AnchorHead.loss_by_feat',
+            'mmdet.models.dense_heads.yolox_head.YOLOXHead.predict_by_feat',  # noqa: E501
+            'mmdet.models.dense_heads.yolox_head.YOLOXHead.loss_by_feat',
         ]))
 
-checkpoint='/nvme/humu/experiments/retina_trt/model_ptq_deploy.pth'
+checkpoint='/mnt/petrelfs/humu/experiments/yolox-s_trt_try/model_ptq_deploy.pth'
+# checkpoint=None
